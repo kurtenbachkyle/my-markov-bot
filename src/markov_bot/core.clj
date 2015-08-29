011c10e18958678c7004366b5271b3fcb192d598
Kyle Kurtenbach <kurtenbachkyle@users.noreply.github.com>
2015-08-29 14:56:54 -0500

    work for chains

--- src/markov_bot/core.clj
+++
@@ -1,6 +0,0 @@
-(ns markov-bot.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
---
+++ src/markov_bot/generator.clj
@@ -0,0 +1,28 @@
+(ns markov-bot.generator
+  (:require [clojure.string :as str]))
+
+(defn word-chain [word-transitions]
+  (reduce 
+    (fn [r t] 
+      (merge-with 
+        clojure.set/union r 
+        (let [[a b c] t] 
+          {[a b] (if c #{c} #{})}))) 
+    {} word-transitions))
+
+(defn text->word-chain [text]
+  (letfn [(word-split [t]
+            (str/split t #"( |\n)"))
+          (words->word-transitions [ws]
+            (partition-all 3 1 ws))
+          (text->word-transitions [t]
+            (words->word-transitions (word-split t)))]
+    (word-chain  (text->word-transitions text))))
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (cond 
+      (empty? suffixes) result
+      :else (let [suffix (first (shuffle suffixes))
+                  new-prefix [(last prefix) suffix]]
+              (recur new-prefix chain (conj result suffix))))))
